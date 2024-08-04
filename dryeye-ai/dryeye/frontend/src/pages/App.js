e1c5b5fe8357741f834e07e8c988ed7155ac66e8
Karishvan new <k1ragunathan@torontomu.ca>
2024-08-04 00:33:46 -0400

    Added route to Automatic prediction

--- dryeye-ai/dryeye/frontend/src/pages/App.js
+++ dryeye-ai/dryeye/frontend/src/pages/App.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
-import StartPage from './StartPage';
-import Home from './Home';
-import Prediction from './Prediction';
-import Dashboard from './Dashboard';
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import StartPage from "./StartPage";
+import Home from "./Home";
+import Prediction from "./Prediction";
+import Dashboard from "./Dashboard";
+import AutomaticPrediction from "./AutomaticPrediction";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   const location = useLocation();
-  const isStartPage = location.pathname === '/';
+  const isStartPage = location.pathname === "/";
 
   return (
     <div className="App">
       {!isStartPage && <Sidebar />} {/* Conditionally render Sidebar */}
-      <div className={`App-body ${isStartPage ? 'StartPage-body' : ''}`}>
+      <div className={`App-body ${isStartPage ? "StartPage-body" : ""}`}>
         <main className="App-content">
           <Routes>
             <Route path="/" element={<StartPage />} />
             <Route path="/home" element={<Home />} />
             <Route path="/prediction" element={<Prediction />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route
+              path="/automatic-prediction"
+              element={<AutomaticPrediction></AutomaticPrediction>}
+            ></Route>
           </Routes>
         </main>
       </div>
