c647845efe36f5508ad28cc75af3608ea0087ac4
Ammar Hakim <ammarhakim57@gmail.com>
2024-08-04 05:44:49 -0400

    Update model.py

--- dryeye-ai/dryeye/main/ml_models/model.py
+++ dryeye-ai/dryeye/main/ml_models/model.py
@@ -8,18 +8,13 @@ rf_class = joblib.load(MODEL_PATH_1)
 MODEL_PATH_2 = 'C:/Users/ammar/OneDrive/Desktop/DryEye AI/DryEye/dryeye-ai/dryeye/main/ml_models/models/drought_rf_reg_model.joblib'
 rf_reg = joblib.load(MODEL_PATH_2)
 
-
-
-
+def preprocess_date(date_str):
+    """Convert the input date string into a pandas datetime object."""
+    return pd.to_datetime(date_str, format='%Y-%m-%d')
 
 def predict(date_str):
-    # Preprocess date
-    print("ENTEREDDDDDDDDDDDDDDDD")
-    date = preprocess_date(date_str)
-    
-    print("TEST_____________")
     # Prepare features
-    X = date
+    X = preprocess_date(date_str)
     
     # Predict drought status
     drought_prediction = rf_class.predict(X)[0]
