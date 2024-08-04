8ae392e4e6a59199ddc9cee2a78b0eaab5a0c119
Ammar Hakim <ammarhakim57@gmail.com>
2024-08-04 05:22:12 -0400

    Update views.py

--- dryeye-ai/dryeye/main/views.py
+++ dryeye-ai/dryeye/main/views.py
@@ -1,11 +1,8 @@
 from rest_framework.decorators import api_view
+from django.http import JsonResponse
 from .ml_models.model import predict
 
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view
-
 @api_view(['POST'])
-@csrf_exempt  # Use with caution, only if you need to exempt this view from CSRF checks
 def predict_view(request):
     station_id = request.data.get('station_id')
     if station_id is not None:
